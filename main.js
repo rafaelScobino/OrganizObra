ad9c04303d9181c2621592ccbe2b3261b2afe353
baltar9 <141790128+baltar9@users.noreply.github.com>
2023-12-24 04:45:06 -0300

    24/12 auto tables

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//Variaveis Globais
+
+
 //Função input currency e função totalq currency
 function numValue(valor){
     valor= valor.replace(/[^\d]/g,'')
@@ -19,12 +22,14 @@ function quinzenaMoeda() {
         style: 'currency',
         currency: 'BRL'})
     }
+
 //Automatizando Tabelas com Inputs - Necessita Otimização
 var funcionario = []
 var counter = 1
 
 function onSubmitFuncionario(event){
     event.preventDefault();
+    obra=obraInput();
     nome = document.getElementById('name').value;
     funcao = document.getElementById('funcao').value;
     valDia = document.getElementById('valorDia').value;
@@ -32,6 +37,7 @@ function onSubmitFuncionario(event){
     totalQ = document.getElementById('totalQ').value;
     pix = document.getElementById('pix').value;
     tipoPix = document.querySelector('input[name="tipoPix"]:checked').value;
+        funcionario.push(obra);
         funcionario.push(nome);
         funcionario.push(funcao);
         funcionario.push(valDia);
@@ -42,18 +48,37 @@ function onSubmitFuncionario(event){
     console.log(funcionario)
 }
 
+function obraInput(){
+    if (document.getElementById('newObra').value !=''){
+        return document.getElementById('newObra').value;
+    }else{
+        return document.getElementById('obraSelect').value;
+    }
+}
+
 function updateTable(){
     console.log(funcionario)
+    console.log(obraInput())
     const tr = document.createElement('tr');
-        tr.setAttribute('id','linha'+counter)
+        tr.setAttribute('id','linha'+ counter)
+    const tr2 = document.createElement('tr');
+        tr2.setAttribute('id','linha'+ obraInput() + counter)
             document.getElementById('tablePag').appendChild(tr);
+            document.getElementById(obraInput()).appendChild(tr2);
     funcionario.forEach(i => {
          const td = document.createElement('td');
             td.innerHTML=i;
                 document.getElementById('linha'+counter).appendChild(td);
     });
+    funcionario.splice(0,1);
+    funcionario.forEach(i => {
+        const td = document.createElement('td');
+           td.innerHTML=i;
+               document.getElementById('linha'+ obraInput() + counter).appendChild(td);
+   });
     funcionario=[];
     counter++;
+    document.getElementById('newObra').value='';
     document.getElementById('name').value = '';
     document.getElementById('pix').value = '';
     document.getElementById('funcao').value = '';
@@ -63,7 +88,64 @@ function updateTable(){
     document.getElementById('name').focus();
 }
 
+//Automação de tabelas por obra
+function newTable(){
+    console.log('Inside newTable function');
+    obra = document.getElementById('newObra').value;
+    if (obra !=''){
+        const optObra = document.createElement('option');
+            optObra.setAttribute('value', obra);
+            optObra.innerHTML = obra;
+                document.getElementById('obraSelect').appendChild(optObra)
+        const table = document.createElement('table');
+            table.setAttribute('id', obra)
+            table.setAttribute('name', obra)
+                document.getElementById('tables').appendChild(table)
+        const newTrHead = document.createElement('tr');
+            newTrHead.setAttribute('id', obra + "headObra")
+                document.getElementById(obra).appendChild(newTrHead)
+        const th = document.createElement('th');
+            th.setAttribute('colspan',"8")
+            th.innerHTML = obra
+                document.getElementById(obra+'headObra').appendChild(th)
+        const newTr = document.createElement('tr');
+            newTr.setAttribute('id', obra + "headRow" )    
+                document.getElementById(obra).appendChild(newTr);
+
+        const nome = document.createElement('th');
+            nome.innerHTML = 'Nome'
+                document.getElementById(obra+'headRow').appendChild(nome);
+        const funcao = document.createElement('th');
+            funcao.innerHTML = 'Função'
+                document.getElementById(obra+'headRow').appendChild(funcao);
+        const diaria = document.createElement('th');
+            diaria.innerHTML = 'Valor Diária'
+                document.getElementById(obra+'headRow').appendChild(diaria);
+        const dias = document.createElement('th');
+            dias.innerHTML = 'Dias Trabalhados'
+                document.getElementById(obra+'headRow').appendChild(dias);
+        const totalQ = document.createElement('th');
+            totalQ.innerHTML = 'Total Quinzena'
+                document.getElementById(obra+'headRow').appendChild(totalQ);
+        const pix = document.createElement('th');
+            pix.innerHTML = 'Pix'
+                document.getElementById(obra+'headRow').appendChild(pix);
+        const tipoPix = document.createElement('th');
+            tipoPix.innerHTML = 'Tipo de pix'
+                document.getElementById(obra+'headRow').appendChild(tipoPix);
+
+        const br = document.createElement('br');
+        const br2 = document.createElement('br');
+            document.getElementById(obra).insertAdjacentElement('afterend', br)
+            document.getElementById(obra).insertAdjacentElement('afterend', br2)
+    }else {
+        return null;
+    }
+}
+
+//Funções onSubmit
 function submitTable(event){
     onSubmitFuncionario(event);
-    updateTable();
+    newTable();
+    updateTable()
 }
